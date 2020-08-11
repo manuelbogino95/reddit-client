505332da8bb70e3423ecebe0e89be19129b079cc
Manuel Bogino <manuelbogino@gmail.com>
2020-08-11 10:56:32 -0300

    Add loader

--- src/containers/PostList/PostList.js
+++ src/containers/PostList/PostList.js
@@ -2,9 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import styles from "./PostList.module.scss";
 import Post from "../../components/Post";
+import Loader from "../../components/Loader";
 
 const PostList = () => {
   const { children } = useSelector((state) => state.posts.posts);
+  const { isFetching } = useSelector((state) => state.posts);
 
   return (
     <div>
@@ -14,7 +16,11 @@ const PostList = () => {
         <button type="button">After</button>
       </div>
       <div className={styles.list}>
-        {children && children.map((post) => <Post post={post} />)}
+        {isFetching ? (
+          <Loader />
+        ) : (
+          children && children.map((post) => <Post post={post} />)
+        )}
       </div>
     </div>
   );
